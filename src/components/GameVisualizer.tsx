d2ce1894ab5ccfe9e89b9c24a2a9b4771fad56d3
Njiall <mbeilles@student.42.fr>
2022-07-31 00:23:31 +0200

    Tweaked styles

--- src/components/GameVisualizer.tsx
+++ src/components/GameVisualizer.tsx
@@ -1,11 +1,47 @@
-import { Box, Card, Center, createStyles, SimpleGrid } from "@mantine/core"
+import {
+  Box,
+  Card,
+  Center,
+  Container,
+  createStyles,
+  SimpleGrid,
+} from "@mantine/core"
 import { IconCircle, IconSquare, IconStar, IconTriangle } from "@tabler/icons"
 import { ReactNode } from "react"
-import { CellModifier, Machine } from "../features/Game"
+import { threadId } from "worker_threads"
+import { Cell, CellModifier, Machine } from "../features/Game"
 
-const useStyles = createStyles((theme) => ({
-  empty: {},
-}))
+const useStyles = createStyles((theme) => {
+  const dark = theme.colorScheme === "dark"
+
+  return {
+    comon: {
+      padding: 6,
+      borderRadius: theme.radius.md,
+      width: 50,
+      height: 50,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      ["& > *"]: {
+        width: "100%",
+        height: "100%",
+      },
+    },
+    red: {
+      background: theme.colors.red[dark ? 8 : 3],
+    },
+    blue: {
+      background: theme.colors.blue[dark ? 8 : 3],
+    },
+    green: {
+      background: theme.colors.green[dark ? 8 : 3],
+    },
+    empty: {
+      background: "transparent",
+    },
+  }
+})
 
 const modifierIcon: Record<CellModifier, ReactNode> = {
   // Robot
@@ -20,31 +56,28 @@ const modifierIcon: Record<CellModifier, ReactNode> = {
 }
 
 export default function GameVisualizer({ machine }: { machine: Machine }) {
+  const { classes, cx } = useStyles()
   const { map } = machine
 
   return (
     <>
       <Center>
-        <Card shadow="xl" radius="lg">
-          <SimpleGrid cols={map.width} spacing="xs">
-            {map.cells.map((cell) => (
-              <Box
-                sx={(theme) => ({
-                  background: theme.colors[cell?.color ?? "gray"][5],
-                  padding: theme.spacing.xs,
-                  borderRadius: theme.radius.md,
-                  width: 50,
-                  height: 50,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                })}
-              >
-                {cell?.modifier && modifierIcon[cell?.modifier]}
-              </Box>
-            ))}
-          </SimpleGrid>
-        </Card>
+        <Container size="xl" p={0}>
+          <Card withBorder radius="lg">
+            <SimpleGrid cols={map.width} spacing={4}>
+              {map.cells.map((cell) => (
+                <Box
+                  className={cx(
+                    classes.comon,
+                    cell?.color ? classes[cell.color] : classes.empty
+                  )}
+                >
+                  {cell?.modifier && modifierIcon[cell?.modifier]}
+                </Box>
+              ))}
+            </SimpleGrid>
+          </Card>
+        </Container>
       </Center>
     </>
   )
